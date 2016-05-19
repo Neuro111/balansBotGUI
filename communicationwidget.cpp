dea993c15b8035565551c0021e3aa90ea7269e17
KW <k.witkowski@optimum.com.pl>
2016-05-19 09:55:20 +0200

    widget komunikacji z mozliwościa wyboru portu i otwieraniem portu

--- communicationwidget.cpp
+++ communicationwidget.cpp
@@ -6,9 +6,64 @@ CommunicationWidget::CommunicationWidget(QWidget *parent) :
     ui(new Ui::CommunicationWidget)
 {
     ui->setupUi(this);
+
+    port = new QSerialPort(this);
+    port->setBaudRate(QSerialPort::Baud115200);
+    port->setDataBits(QSerialPort::Data8);
+    port->setParity(QSerialPort::NoParity);
+    port->setStopBits(QSerialPort::OneStop);
+
+
+    QList<QSerialPortInfo> availablePorts = QSerialPortInfo::availablePorts();
+    for(int i=0; i<availablePorts.count(); i++)
+    {
+        ui->portListComboBox->addItem(availablePorts.at(i).portName());
+    }
+    ui->portListComboBox->addItem("<odśwież>");
 }
 
 CommunicationWidget::~CommunicationWidget()
 {
     delete ui;
+}
+
+void CommunicationWidget::on_OpenButton_toggled(bool checked)
+{
+    if(checked)
+    {
+        port->setPortName(ui->portListComboBox->currentText());
+        if (port->open(QIODevice::ReadWrite)) {
+            ui->statusLabel->setText("Port otwarty");
+            ui->portListComboBox->setEnabled(false);
+            //connect(serial,SIGNAL(readyRead()),this,SLOT(readData()));
+        } else {
+            ui->statusLabel->setText("Nie można otworzyć portu" + port->errorString());
+            ui->OpenButton->setChecked(false);
+        }
+    }
+    else
+    {
+        if (port->isOpen())
+        {
+            port->close();
+        }
+        //disconnect(serial,SIGNAL(readyRead()),this,SLOT(readData()));
+        ui->portListComboBox->setEnabled(true);
+    }
+}
+
+void CommunicationWidget::on_portListComboBox_activated(const QString &arg1)
+{
+    if(arg1 == "<odśwież>")
+    {
+        ui->portListComboBox->clear();
+        QList<QSerialPortInfo> availablePorts = QSerialPortInfo::availablePorts();
+        for(int i=0; i<availablePorts.count(); i++)
+        {
+            ui->portListComboBox->addItem(availablePorts.at(i).portName());
+        }
+        ui->portListComboBox->addItem("<odśwież>");
+        ui->portListComboBox->setCurrentIndex(0);
+    }
+
 }
--- communicationwidget.h
+++ communicationwidget.h
@@ -2,6 +2,9 @@
 #define COMMUNICATIONWIDGET_H
 
 #include <QWidget>
+#include <QtSerialPort/QSerialPortInfo>
+#include <QtSerialPort/QSerialPort>
+#include <QDebug>
 
 namespace Ui {
 class CommunicationWidget;
@@ -15,8 +18,15 @@ public:
     explicit CommunicationWidget(QWidget *parent = 0);
     ~CommunicationWidget();
 
+private slots:
+
+    void on_OpenButton_toggled(bool checked);
+
+    void on_portListComboBox_activated(const QString &arg1);
+
 private:
     Ui::CommunicationWidget *ui;
+    QSerialPort *port;
 };
 
 #endif // COMMUNICATIONWIDGET_H
